1afeabe35356ccf6a9452cc8610115a8f80e66f2
Conor McGrath <conormcgrath123@gmail.com>
2023-03-11 12:28:32 +0000

    section 1 - getting started

---
+++ 1-getting-started/react-app/src/App.tsx
@@ -0,0 +1,13 @@
+import Message from "./Message";
+
+function App() {
+  //see self closing tag
+  //<Message></Message> can become <Message />
+  return (
+    <div>
+      <Message />
+    </div>
+  );
+}
+
+export default App;
---
+++ 1-getting-started/react-app/src/Message.tsx
@@ -0,0 +1,13 @@
+//pascalCasing
+
+function Message() {
+  //JSX: JavaScro[t XML
+  const name = "Conor";
+
+  //we can execute any javascript code in here.
+
+  if (name) return <h1>Hello {name}</h1>;
+  else return <h1>Hello World</h1>;
+}
+
+export default Message;
