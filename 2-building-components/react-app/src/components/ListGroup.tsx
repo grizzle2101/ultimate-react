aefff63f6c85f72bef50c0371f0b6c99cfe466ac
Conor McGrath <conormcgrath123@gmail.com>
2023-03-11 16:15:15 +0000

    2-building-components - conditional rendering

--- 2-building-components/react-app/src/components/ListGroup.tsx
+++ 2-building-components/react-app/src/components/ListGroup.tsx
@@ -1,9 +1,20 @@
 function ListGroup() {
-  const items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  var items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  items = [];
 
+  //method 1 - as a constant
+  const message = items.length === 0 ? <p>No Items Found</p> : null;
+
+  //method 2 - as a function
+  const getMessage = () => {
+    return items.length === 0 ? <p>No Items Found</p> : null;
+  };
+
+  //method 3 - using coparison operators
   return (
     <>
       <h1>List</h1>
+      {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
