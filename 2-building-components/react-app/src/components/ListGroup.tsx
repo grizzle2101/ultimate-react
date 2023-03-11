c3834edbe258e4eb07387d693b7a9f61bbdce607
Conor McGrath <conormcgrath123@gmail.com>
2023-03-11 22:34:11 +0000

    2-building-components - managing state

--- 2-building-components/react-app/src/components/ListGroup.tsx
+++ 2-building-components/react-app/src/components/ListGroup.tsx
@@ -1,9 +1,10 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   var items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
 
-  const handleClick = (event: MouseEvent) => console.log("clicked - ", event);
+  // state hook
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
@@ -11,7 +12,17 @@ function ListGroup() {
       {items.length === 0 && <p>No Items Found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
-          <li className="list-group-item" key={item} onClick={handleClick}>
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
