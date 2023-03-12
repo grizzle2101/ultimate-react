6bcc21e9ac433c770df92c5ed36b490dc4118a71
Conor McGrath <conormcgrath123@gmail.com>
2023-03-12 19:42:27 +0000

    2-building-components - excercise 2

--- 2-building-components/react-app/src/App.tsx
+++ 2-building-components/react-app/src/App.tsx
@@ -1,18 +1,22 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import Button from "./components/Button";
 
 function App() {
-  const onClick = () => {
-    console.log("DA CLICK");
-  };
-
   const color = "danger";
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <div>
-      <Button color={color} onClick={onClick}>
-        Data
-      </Button>
-    </div>
+    <>
+      {showAlert && (
+        <Alert onClose={() => setShowAlert(false)}> Button was clicked! </Alert>
+      )}
+      <div>
+        <Button color={color} onClick={() => setShowAlert(true)}>
+          Raise Alert
+        </Button>
+      </div>
+    </>
   );
 }
 
--- 2-building-components/react-app/src/components/Alert.tsx
+++ 2-building-components/react-app/src/components/Alert.tsx
@@ -2,10 +2,26 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  onClose: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-  return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClose }: Props) => {
+  return (
+    <>
+      <div>
+        <div className="alert alert-primary alert-dismissible">{children}</div>
+        <button
+          type="button"
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close"
+          onClick={onClose}
+        >
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+    </>
+  );
 };
 
 export default Alert;
