e692dd040525b485c2f1f50d6f55e524c36ab221
Conor McGrath <conormcgrath123@gmail.com>
2023-03-12 12:03:26 +0000

    2-building-components - functions via props

--- 2-building-components/react-app/src/App.tsx
+++ 2-building-components/react-app/src/App.tsx
@@ -3,9 +3,17 @@ import ListGroup from "./components/ListGroup";
 function App() {
   var items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
 
+  const handleSelectItem = (item: string) => {
+    console.log("item - ", item);
+  };
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <ListGroup
+        onSelectItem={handleSelectItem}
+        items={items}
+        heading="Cities"
+      />
     </div>
   );
 }
