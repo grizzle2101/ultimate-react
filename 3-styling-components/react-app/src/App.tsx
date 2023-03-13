145aaa2e04de622eaa959cb34684d213b4feab4d
Conor McGrath <conormcgrath123@gmail.com>
2023-03-13 18:31:55 +0000

    3-styling-components - vanilla css

---
+++ 3-styling-components/react-app/src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
+
+function App() {
+  var items = ["New York", "San Fransico", "Tokyo", "London", "Paris"];
+  const handleSelectItem = (item: string) => {
+    console.log("item - ", item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        onSelectItem={handleSelectItem}
+        items={items}
+        heading="Cities"
+      />
+    </div>
+  );
+}
+
+export default App;
