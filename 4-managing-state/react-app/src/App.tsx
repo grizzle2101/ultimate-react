ad3cce691271b1ade4d0d4ea0192221e52d7027a
Conor McGrath <conormcgrath123@gmail.com>
2023-03-23 20:15:20 +0000

    4-managing state - updating objects

--- 4-managing-state/react-app/src/App.tsx
+++ 4-managing-state/react-app/src/App.tsx
@@ -1,11 +1,21 @@
 import { useState } from "react";
 import "./App.css";
-import Message from "./components/Message/Message";
+import Button from "./components/Button/Button";
 
 function App() {
+  const [drink, setDrink] = useState({
+    title: "Americano",
+    price: 5,
+  });
+
+  const handleClick = () => {
+    setDrink({ ...drink, price: drink.price + 1 });
+  };
+
   return (
     <div>
-      <Message>Test</Message>
+      {drink.price}
+      <Button onClick={handleClick}>Test Button</Button>
     </div>
   );
 }
