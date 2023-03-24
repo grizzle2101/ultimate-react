deebfac3803b9ca8b8ed6171b74fb15edbe6f593
Conor McGrath <conormcgrath123@gmail.com>
2023-03-24 11:15:32 +0000

    4-managing state - updating object arrays

--- 4-managing-state/react-app/src/App.tsx
+++ 4-managing-state/react-app/src/App.tsx
@@ -3,27 +3,18 @@ import "./App.css";
 import Button from "./components/Button/Button";
 
 function App() {
-  const [tags, setTags] = useState(["happy", "cheerful"]);
+  const [bugs, setBugs] = useState([
+    { id: 1, title: "bug 1", fixed: false },
+    { id: 2, title: "bug 2", fixed: false },
+  ]);
 
   const handleClick = () => {
-    //so say we want to update the address..
-    //setTags([tags.push('newOne')]);//tags.push - NO
-
-    //Add
-    setTags([...tags, "newOne"]);
-
-    //remove
-    setTags(tags.filter((tag) => tag !== "happy"));
-
-    //update
-    //method 1 - copy array
-    //methood 2 - map
-    setTags(tags.map((tag) => (tag === "happy" ? "happiness" : tag)));
+    setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
   };
 
   return (
     <div>
-      {JSON.stringify(tags)}
+      {JSON.stringify(bugs)}
       <Button onClick={handleClick}>Test Button</Button>
     </div>
   );
