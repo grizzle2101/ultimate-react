8a51455000bf84ea6e0a93611b0082d882b8b6e8
Conor McGrath <conormcgrath123@gmail.com>
2023-03-25 22:00:14 +0000

    4-managing state - sharing state between components

--- 4-managing-state/react-app/src/App.tsx
+++ 4-managing-state/react-app/src/App.tsx
@@ -1,35 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button/Button";
-import produce from "immer";
+import NavBar from "./components/NavBar/NavBar";
+import Cart from "./components/Cart/Cart";
 
 function App() {
-  const [bugs, setBugs] = useState([
-    { id: 1, title: "bug 1", fixed: false },
-    { id: 2, title: "bug 2", fixed: false },
-  ]);
+  const [cartItems, setCartItems] = useState(["one", "two", "three"]);
 
-  const handleClick = () => {
-    //draft is proxy object that records changes (a copy of bugs).
-    setBugs(
-      produce((draft) => {
-        const bug = draft.find((bug) => bug.id === 1);
-        if (bug) bug.fixed = true;
-      })
-    );
-    //setBugs(bugs.map((bug) => (bug.id === 1 ? { ...bug, fixed: true } : bug)));
+  const clearCart = () => {
+    setCartItems([]);
   };
 
   return (
-    <div>
-      {bugs.map((bug) => (
-        <p key={bug.id}>
-          {bug.title} {bug.fixed ? "Fixed" : "New"}
-        </p>
-      ))}
-      {JSON.stringify(bugs)}
-      <Button onClick={handleClick}>Test Button</Button>
-    </div>
+    <>
+      <div>
+        <NavBar cartItemsCount={cartItems.length}></NavBar>
+        <Cart cartItems={cartItems} onClear={clearCart}></Cart>
+      </div>
+    </>
   );
 }
 
