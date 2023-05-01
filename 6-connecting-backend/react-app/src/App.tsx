18e47ea4db68fe2f85b2a7afa828fef726e88e69
Conor McGrath <conormcgrath123@gmail.com>
2023-05-01 13:30:33 +0100

    6-connecting-backend- delete user

--- 6-connecting-backend/react-app/src/App.tsx
+++ 6-connecting-backend/react-app/src/App.tsx
@@ -34,13 +34,40 @@ function App() {
     };
   }, []);
 
+  const deleteUser = (user: User) => {
+    //2 options - optimistic or
+    //optimistic - update the UI, call the server. (faster)
+    //pessimistic update - call the server, update the UI. (slower)
+    const originalUsers = [...users];
+
+    setUsers(users.filter((u) => u.id !== user.id));
+    axios
+      .delete("https://jsonplaceholder.typicode.com/xusers/" + user.id)
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
+
+  //add button
   return (
     <>
       {isLoading && <div className="spinner-border"></div>}
       {error && <p className="text-danger">{error}</p>}
-      <ul>
+      <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id}> {user.name}</li>
+          <li
+            className="list-group-item d-flex justify-content-between"
+            key={user.id}
+          >
+            {user.name}
+            <button
+              className="btn btn-outline-danger"
+              onClick={() => deleteUser(user)}
+            >
+              Delete
+            </button>
+          </li>
         ))}
       </ul>
     </>
