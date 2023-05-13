d4234b7c1817b0ba27e48afd7758dc4fa68efed6
Conor McGrath <conormcgrath123@gmail.com>
2023-05-13 11:42:06 +0100

    6-connecting-backend- custom fetching hook

--- 6-connecting-backend/react-app/src/App.tsx
+++ 6-connecting-backend/react-app/src/App.tsx
@@ -1,31 +1,8 @@
-import { useEffect, useState } from "react";
-import { CanceledError } from "./services/api-client";
 import userService, { User } from "./services/userService";
+import useUsers from "./hooks/useUsers";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    const { request, cancel } = userService.getAll<User>();
-
-    request
-      .then((res) => {
-        setLoading(false);
-        setUsers(res.data);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setLoading(false);
-        setError(err.message);
-      });
-
-    return () => {
-      cancel();
-    };
-  }, []);
+  const { users, error, isLoading, setUsers, setError } = useUsers();
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
---
+++ 6-connecting-backend/react-app/src/hooks/useUsers.ts
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import userService, { User } from "../services/userService";
+import { CanceledError } from "../services/api-client";
+
+const useUsers = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const { request, cancel } = userService.getAll<User>();
+
+    request
+      .then((res) => {
+        setLoading(false);
+        setUsers(res.data);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setLoading(false);
+        setError(err.message);
+      });
+
+    return () => {
+      cancel();
+    };
+  }, []);
+
+  //return state variables for use in component.
+  return { users, error, isLoading, setUsers, setError };
+};
+
+export default useUsers;
