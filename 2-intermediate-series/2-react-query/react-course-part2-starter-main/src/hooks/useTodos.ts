9673a70844b1bb135d0f1f6a9eb331185eedc55a
Conor McGrath <conormcgrath123@gmail.com>
2024-01-14 11:23:07 +0000

    intermediate - 2 - react query - custom hook

---
+++ 2-intermediate-series/2-react-query/react-course-part2-starter-main/src/hooks/useTodos.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const useTodos = () => {
+  const fetchTodos = () => {
+    console.log("getting...");
+    return axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+  };
+
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos", { completed: true }],
+    queryFn: fetchTodos,
+  });
+};
+
+export default useTodos;
