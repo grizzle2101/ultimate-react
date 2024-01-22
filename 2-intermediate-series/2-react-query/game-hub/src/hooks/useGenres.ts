e64dda4349db41e0cbf6af671e9246230219d4ff
Conor McGrath <conormcgrath123@gmail.com>
2024-01-22 19:51:55 +0000

    intermediate - 2 - react query -  genres excercise

---
+++ 2-intermediate-series/2-react-query/game-hub/src/hooks/useGenres.ts
@@ -0,0 +1,28 @@
+import { useQuery } from "@tanstack/react-query";
+import { GENRE_CACHE_KEY } from "../constants";
+import apiClient from "../services/api-client";
+import { FetchResponse } from "./useData";
+import genres from "../data/genres";
+
+export interface Genre {
+  id: number;
+  name: string;
+  image_background: string;
+}
+
+const getGenres = () => {
+  return apiClient
+    .get<FetchResponse<Genre>>("/genres")
+    .then((res: any) => res.data);
+};
+
+const useGenres = () => {
+  return useQuery({
+    queryKey: GENRE_CACHE_KEY,
+    queryFn: getGenres,
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: genres.length, results: genres },
+  });
+};
+
+export default useGenres;
