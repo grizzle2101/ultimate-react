c9476fbbcb5793dc6c64b506e490f1256c385496
Conor McGrath <conormcgrath123@gmail.com>
2024-01-23 11:08:14 +0000

    intermediate - 2 - react query -  platforms excercise

--- 2-intermediate-series/2-react-query/game-hub/src/constants.ts
+++ 2-intermediate-series/2-react-query/game-hub/src/constants.ts
@@ -1,1 +1,2 @@
 export const GENRE_CACHE_KEY = ["genres"];
+export const PLATFORM_CACHE_KEY = ["platforms"];
--- 2-intermediate-series/2-react-query/game-hub/src/hooks/usePlatforms.ts
+++ 2-intermediate-series/2-react-query/game-hub/src/hooks/usePlatforms.ts
@@ -1,4 +1,8 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { PLATFORM_CACHE_KEY } from "../constants";
+import { FetchResponse } from "./useData";
 
 interface Platform {
   id: number;
@@ -6,6 +10,19 @@ interface Platform {
   slug: string;
 }
 
-const usePlatforms = () => ({ data: platforms, isLoading: false, error: null });
+const getPlatforms = () => {
+  return apiClient
+    .get<FetchResponse<Platform>>("/platforms/lists/parents")
+    .then((res: any) => res.data);
+};
+
+const usePlatforms = () => {
+  return useQuery<FetchResponse<Platform>, Error>({
+    queryKey: PLATFORM_CACHE_KEY,
+    queryFn: getPlatforms,
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
+};
 
 export default usePlatforms;
