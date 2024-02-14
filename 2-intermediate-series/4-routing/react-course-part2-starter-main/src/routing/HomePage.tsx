a7ea042770247625620465a3d691afe93a50da6c
Conor McGrath <conormcgrath123@gmail.com>
2024-02-14 21:26:11 +0000

    intermediate - 4 - routing - handling errors

--- 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/HomePage.tsx
+++ 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/HomePage.tsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  //throw new Error("test");
   return (
     <>
       <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-        Incidunt, mollitia!
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt,
+        mollitia!
       </p>
       <Link to="/users">Users</Link>
     </>
--- 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/routes.tsx
+++ 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/routes.tsx
@@ -4,16 +4,25 @@ import Layout from "./Layout";
 import UserDetailPage from "./UserDetailPage";
 import UserListPage from "./UserListPage";
 import UserPage from "./userPage";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
-    {path: '/', element: <Layout />, children: [
-    {index: true, element: <HomePage />}, //index = true, means this homepage.
-    {path: '/users', element: <UserListPage />, children: [
-        {path: ':id/:name', element: <UserPage />}, //UserPage only appears on correct link
-    ]},
-    {path: '/users/:id', element: <UserDetailPage />},
-    ]}, 
+  {
+    path: "/",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <HomePage /> }, //index = true, means this homepage.
+      {
+        path: "/users",
+        element: <UserListPage />,
+        children: [
+          { path: ":id/:name", element: <UserPage /> }, //UserPage only appears on correct link
+        ],
+      },
+      { path: "/users/:id", element: <UserDetailPage /> },
+    ],
+  },
 ]);
-
 
 export default router;
