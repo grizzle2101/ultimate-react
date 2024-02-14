22cb46aab2d444fa51420a249429f95bb5fc4ab7
Conor McGrath <conormcgrath123@gmail.com>
2024-02-14 18:12:24 +0000

    intermediate - 4 - routing - getting data from route

--- 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/UserDetailPage.tsx
+++ 2-intermediate-series/4-routing/react-course-part2-starter-main/src/routing/UserDetailPage.tsx
@@ -1,4 +1,18 @@
+import { useLocation, useParams, useSearchParams } from "react-router-dom";
+
 const UserDetailPage = () => {
+  const params = useParams(); 
+  console.log('params - ', params);
+
+  const [searchParams, setSearchParams] = useSearchParams()
+  console.log('searchParams - ', searchParams.toString());
+
+  const location = useLocation();
+  console.log('location - ', location);
+
+
+  console.log('name - ', searchParams.get('name'));
+
   return <p>User</p>;
 };
 
